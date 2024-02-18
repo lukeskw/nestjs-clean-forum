9157a6937879cdd996798df206b63cfb9ecc4daf
lukeskw <lucasporfirioa@gmail.com>
2024-02-18 19:34:04 -0300

    feat: implementing create account controller

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { hash } from 'bcryptjs'
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+const createAccountSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountSchema>
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountSchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = createAccountSchema.parse(body)
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('Email already in use')
+    }
+
+    const hashedPassword = await hash(password, 10)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
