242cf8044fb79b062e978e1bfa3c5d8fb254f5e7
lukeskw <lucasporfirioa@gmail.com>
2024-02-19 21:26:04 -0300

    refactor: refactoring repositories to be abstract class due to nestjs service container problem

--- src/domain/forum/application/repositories/question-comments.repository.ts
+++ src/domain/forum/application/repositories/question-comments.repository.ts
@@ -1,11 +1,11 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
 
-export interface QuestionCommentsRepository {
-  findById(id: string): Promise<QuestionComment | null>
-  create(question: QuestionComment): Promise<void>
-  delete(question: QuestionComment): Promise<void>
-  findManyByQuestionId(
+export abstract class QuestionCommentsRepository {
+  abstract findById(id: string): Promise<QuestionComment | null>
+  abstract create(question: QuestionComment): Promise<void>
+  abstract delete(question: QuestionComment): Promise<void>
+  abstract findManyByQuestionId(
     questionId: string,
     params: PaginationParams,
   ): Promise<QuestionComment[]>
